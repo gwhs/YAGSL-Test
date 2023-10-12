9d7f2f65d235acd0e79b591c2c171726cb8c9e79
Chvoong24 <89755355+Chvoong24@users.noreply.github.com>
2023-10-12 16:25:56 -0700

    changed canbus type

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -45,7 +45,7 @@ public class Robot extends TimedRobot
   @Override
   public void robotInit()
   {
-    PathPlannerServer.startServer(5811);
+    PathPlannerServer.startServer(5507);
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
